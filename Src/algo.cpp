f2ecb8bd29fff6c0bf84d5bb895b539e1e456707
scarl <scarlet25151@gmail.com>
2019-12-15 04:04:33 -0500

    Update Algo.cpp

--- Src/algo.cpp
+++ Src/algo.cpp
@@ -2,9 +2,27 @@
 
 #include "algo.h"
 #include "Arduino.h"
+#include <vector>
 
 #define ALGO_WIFI_N 32.5    //N = 10 * n ,其中n为环境衰减因子，3.25-4.5
 #define ALGO_WIFI_A -45     //接收机和发射机间隔1m时的信号强度
+
+#define GRID_LENGTH 400
+#define GRID_SIZE 50
+
+Algo::Algo(void) {
+    int grid_h = GRID_LENGTH / GRID_SIZE;
+    int grid_w = GRID_LENGTH / GRID_SIZE;
+
+    grid.resize(grid_h);
+    for (int i = 0; i < grid_h; ++i) {
+        grid[i].resize(grid_w);
+    }
+    init_x = grid_h / 2;
+    init_y = grid_w / 2;
+    prevPosition.first = init_x;
+    prevPosition.second = init_y;
+}
 
 float Algo::wifi2Distance(int strengh)
 {
@@ -13,10 +31,41 @@ float Algo::wifi2Distance(int strengh)
     return distance;
 }
 
-int Algo::distance2Point(int x, int y, const int *position, int refDistance)
-{
-    double distance = sqrt(sq(x - *position) + sq(y - *(position + 1)));
-    
-    // todo
-    return 0;
+void Algo::updatePosition() {
+    // TODO: pase in the movement;
+    pair<int, int> newPosition;
+    prevPosition = currPosition;
+    currPosition = newPosition;
 }
+
+float Algo::angle2north() {
+    return atan((currPosition.second - prevPosition.second) / (currPosition.first - prevPosition.first));
+}
+
+void Algo::updateGrid(int rx, int ry, int r) {
+    double tol = 0.1;
+    for (int x = 0; x < grid.size(); x++) {
+        for (int y = 0; y < grid[0].size(); y++) {
+            if ((double) abs(sq(x - rx) + sq(y - ry) - sq(r))< tol) {
+                grid[x][y] += 1;
+            }
+        }
+    }
+}
+
+pair<int, int> Algo::findMax() {
+    int maxRSSI = -1;
+    pair<int, int> coordinate(-1, -1);
+    for (int x = 0; x < grid.size(); x++) {
+        for (int y = 0; y < grid[0].size(); y++) {
+            if (grid[x][y] > maxRSSI) {
+                maxRSSI = grid[x][y];
+                coordinate.first = x;
+                coordinate.second = y;
+            }
+        }
+    }
+    return coordinate;
+}
+
+
--- Src/algo.h
+++ Src/algo.h
@@ -1,13 +1,37 @@
 // algo.h
+#include <vector>
+
+using namespace std;
 
 #ifndef ALGO_H
 #define ALGO_H
+#define GRID_LENGTH
+#define GRID_SIZE
 
 class Algo
 {
 public:
+    Algo();
     static float wifi2Distance(int strength);
-	static int distance2Point(int x, int y, const int *position, int refDistance);
+	
+    static int distance2Point(int x, int y, const int *position, int refDistance);
+    
+    void updateGrid(int x, int y, int rad);
+    
+    void updatePosition();
+    
+    static float angle2north();
+    
+    static pair<int, int> findMax(); 
+private:
+    
+    static vector<vector<int>> grid; // record the grid
+    
+    static int init_x, init_y; // initial the position
+    
+    static int moveCount; // count the movement
+    
+    static pair<int, int> currPosition, prevPosition;
 };
 
 #endif
